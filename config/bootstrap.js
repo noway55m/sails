44a8bb98fde55ecc77295a51069bccb3335e60ee
frank.hsu <noway55m@gmail.com>
2013-12-10 21:25:46 +0800

    add sample floor files.

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto'), 
 	log = require('log4js').getLogger(), 
-	User = require('../model/user');
+	User = require('../model/user'),
+	Building = require('../model/building');
 
 module.exports = function() {
 
@@ -12,7 +13,7 @@ module.exports = function() {
 // Function for create default administrator
 function createDefaultUser() {
 	
-	// Check duplicate user
+	// Generate default administrator
 	User.findOne({
 
 		username : "admin"
@@ -25,6 +26,53 @@ function createDefaultUser() {
 		if (user) {
 
 			log.info("Default user exist already");
+
+			// Generate default 'MyHome' building
+			Building.findOne({
+
+				name: 'MyHome',
+				userId: user._id
+
+			}, function(err, building){
+
+				if(err)
+					log.error(err);
+
+				if(building){
+
+					log.info("Default building exist already");
+
+				} else {
+
+					new Building({
+
+						name: "MyHome",						
+						desc: "This is sample project",
+						downfloor: 0,
+						upfloor: 1,
+						pub: true,
+						userId: user._id					
+
+					}).save(function(err, building){
+
+						if(err)
+							log.err(err);
+
+						if(building){
+
+							building.mapzip = "Sample/map.zip";
+							building.mapzipUpdateTime = new Date();
+							building.save();
+								
+						}
+
+					});
+
+					log.info('Default building has been generated successfully');
+
+				}
+
+			});
 
 		} else {
 
--- config/config.js
+++ config/config.js
@@ -4,10 +4,12 @@ module.exports = {
 		
 	resourcePath : "./resource/",
 
+	/******** Folder path of Building and floor files  ********/
 	imagePath : "public/client-image",
 	
 	mapInfoPath: "resource/map-info",
 
+	/******** Download sdk, online doc and sample code path ********/
 	androidSdkPath: "resource/sails-relative-res/android/SAILS_SDK_V1.1_Published.jar",
 
 	iosSdkPath: "resource/sails-relative-res/ios/",
@@ -22,6 +24,7 @@ module.exports = {
 	
 	sampleBuildingPath: "resource/map-info/sample",
 	
+	/******** AppId and AppSecret of Facebook, twitter and google+ ********/	
 	facebookAppKey: "191009537749511",
 	
 	facebookAppSecret: "bd632d3cae63b4ad4314a7238dd2aa19",
@@ -33,9 +36,11 @@ module.exports = {
 	googleAppKey: "369163861143.apps.googleusercontent.com",
 	
 	googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
-	
+
+	/******** Default cookie duration ********/		
 	defaultCookieDuration: 604800, // 7 days: 604800 <=> 60 * 60 * 24 * 7
 
+	/******** Default token duration ********/	
 	defaultTokenDuration: 86400 // 1 day: 86400 <=> 60 * 60 * 24
 	
 };
