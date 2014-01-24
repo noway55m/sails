6f550bf6e076ae5031c79ad32885f8b695730366
frank.hsu <noway55m@gmail.com>
2014-01-24 16:27:20 +0800

    Add feedback controller. Add Google analytics lib.

---
+++ routes/feedback.js
@@ -0,0 +1,54 @@
+var log = require('log4js').getLogger(),
+	http = require('http'),
+    utilityS = require("./utility.js"),
+    mkdirp = require("mkdirp"),
+	Feedback = require("../model/feedback"),
+    crypto = require('crypto'),
+    fs = require('fs'),
+	path = require('path'),
+	util = require('util'),
+	config = require('../config/config');
+
+// Static variable
+var	errorResInfo = utilityS.errorResInfo,
+	mapinfo_path = "/" + config.mapInfoPath,
+	image_path = config.imagePath;
+
+// POST Page for show specific building
+exports.comment = function(req, res) {
+
+    if( req.body.name && req.body.comment ) {
+
+        new Feedback({
+
+            name: req.body.name,
+            email: req.body.email,
+            comment: req.body.comment,
+            createdTime: new Date()
+
+        }).save(function( err, feedback ){
+
+            if(err) {
+
+                log.error(err);
+                res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+                    msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+                });     
+
+            } else {
+
+                res.json( errorResInfo.SUCCESS.code, feedback );
+
+            }
+
+        });
+
+    } else {
+      
+        res.json( errorResInfo.INCORRECT_PARAMS.code , { 
+            msg: errorResInfo.INCORRECT_PARAMS.msg
+        });        
+
+    }
+
+};
