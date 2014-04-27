5ea91932e782ab59823faf3f3028364fd5c45eb0
frank.hsu <noway55m@gmail.com>
2014-04-27 15:47:04 +0800

    Add developer application apis.

--- routes/developer.js
+++ routes/developer.js
@@ -1,7 +1,7 @@
 var log = require('log4js').getLogger(),
 	http = require('http'),
     utilityS = require("./utility.js"),
-	User = require("../model/user"),
+	DeveloperApplication = require("../model/developerApplication"),
 	path = require('path'),
 	util = require('util'),
 	config = require('../config/config');
@@ -13,7 +13,7 @@ var	errorResInfo = utilityS.errorResInfo,
 
 // GET Page for show apps of developer
 exports.appIndex = function(req, res) {
-	res.render("building/building-show.html");
+	res.render("developer/app-index.html");
 };
 
 // GET Interface for list apps of developer
@@ -22,16 +22,186 @@ exports.appList = function(req, res) {
 
 // POST Interface for create new app
 exports.appCreate = function(req, res) {
+
+	if(req.body.verifier && req.body.type) {
+
+		new DeveloperApplication({
+
+			apiKey: DeveloperApplication.genApiKey(),
+			type: req.body.type,
+			verifier: req.body.verifier,
+			createdTime: new Date(),
+			updatedTime: new Date(),			
+			userId: req.user._id
+
+		}).save(function(err, developerApplication){
+
+			if(err) {
+
+				log.error(error);
+				res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+					msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+				}); 				
+
+			} else {
+
+				res.json( errorResInfo.SUCCESS.code, developerApplication );
+
+			}
+
+		});
+
+	} else {
+
+		res.json( errorResInfo.INCORRECT_PARAMS.code , { 
+			msg: errorResInfo.INCORRECT_PARAMS.msg
+		}); 
+
+	}
+
 };
 
 // POST Interface for update specific app
 exports.appUpdate = function(req, res) {
+
+	if(req.body._id && req.body.verifier) { 
+
+		DeveloperApplication.findById(req.body._id, function(err, developerApplication) {
+
+			if(err) {
+
+				log.error(error);
+				res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+					msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+				}); 
+
+			} else {
+
+				developerApplication.verifier = req.body.verifier;
+				developerApplication.save(function(err, developerApplication){
+
+					if(err) {
+
+						log.error(error);
+						res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+							msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+						}); 
+
+					} else {
+
+						res.json( errorResInfo.SUCCESS.code, developerApplication);
+
+					}
+
+				});
+
+			}
+
+		});
+
+
+	} else {
+
+		res.json( errorResInfo.INCORRECT_PARAMS.code , { 
+			msg: errorResInfo.INCORRECT_PARAMS.msg
+		});
+
+	}
+
 };
 
 // POST Interface for delete specific app
 exports.appDelete = function(req, res) {
+
+	if(req.body._id) {	
+
+		DeveloperApplication.findById(req.body._id, function(err, developerApplication) {
+
+			if(err) {
+
+				log.error(error);
+				res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+					msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+				}); 
+
+			} else {
+
+				developerApplication.remove(function(err){
+
+					if(err) {
+
+						log.error(error);
+						res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+							msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+						}); 
+
+					} else {
+
+						res.json( errorResInfo.SUCCESS.code, { _id: req.body._id });
+
+					}
+
+				});
+
+			}
+
+		});
+
+
+	} else {
+
+		res.json( errorResInfo.INCORRECT_PARAMS.code , { 
+			msg: errorResInfo.INCORRECT_PARAMS.msg
+		}); 
+
+	}
+
 };
 
 // POST Interface for refresh app token
-exports.appRefreshToken = function(req, res) {
+exports.appRegenerateKey = function(req, res) {
+
+	if(req.body._id) {
+
+		DeveloperApplication.findById(req.body._id, function(err, developerApplication) {
+
+			if(err) {
+
+				log.error(error);
+				res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+					msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+				}); 
+
+			} else {
+
+				developerApplication.apiKey = DeveloperApplication.genApiKey();
+				developerApplication.save(function(err, developerApplication){
+
+					if(err) {
+
+						log.error(error);
+						res.json( errorResInfo.INTERNAL_SERVER_ERROR.code , { 
+							msg: errorResInfo.INTERNAL_SERVER_ERROR.msg
+						}); 
+
+					} else {
+
+						res.json( errorResInfo.SUCCESS.code, developerApplication);
+
+					}
+
+				});
+
+			}
+
+		});
+
+	} else {
+
+		res.json( errorResInfo.INCORRECT_PARAMS.code , { 
+			msg: errorResInfo.INCORRECT_PARAMS.msg
+		}); 
+
+	}
+
 };
