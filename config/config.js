2cbb9d44c003033d464ec53f182424dc53b77530
frank.hsu <noway55m@gmail.com>
2014-05-10 18:12:12 +0800

    Add config with different configuration.

--- config/config.js
+++ config/config.js
@@ -23,22 +23,8 @@ var configuration = {
 
 	defaultIosSdkVersion: 1.2,
 
-
 	/******** Main sample building folder  ********/		
 	sampleBuildingPath: "resource/map-info/Sample",
-
-	/******** AppId and AppSecret of Facebook, twitter and google+ ********/	
-	facebookAppKey: "191009537749511",
-	
-	facebookAppSecret: "bd632d3cae63b4ad4314a7238dd2aa19",
-	
-	twitterAppKey: "T9Ovg7MAiehY4XUPTfZzw",
-	
-	twitterAppSecret: "zcB3JUdQlCslW7546OdropON116vENwx84E0K8bloI",
-	
-	googleAppKey: "369163861143.apps.googleusercontent.com",
-	
-	googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
 
 	/******** Default cookie duration ********/		
 	defaultCookieDuration: 604800, // 7 days: 604800 <=> 60 * 60 * 24 * 7
@@ -56,18 +42,10 @@ var configuration = {
 	/******** Pagination offset per page ********/	
 	pageOffset: 30,
 
-	/******** Google Analystic Relative ********/	
-	gaAccountIdTest1: "UA-50387624-1", // chewinggun.net - frank.hsu
-
-	gaAccountIdTest2: "UA-28254038-2", // chewinggun.net - frank.hsu
-
-	gaAccountId: "UA-47307984-1", // cloud.sailtech.com
-
 	/******** Recaptcha public and private key ********/
 	recaptchaPublicKey: "6Lco-u0SAAAAAM_vZInahzRKgLZMmHN_zJII7QU5",
 
-	recaptchaPrivateKey: "6Lco-u0SAAAAAASO_2mDmdc4aaHP2p4jFBa6hhZ9"
-
+	recaptchaPrivateKey: "6Lco-u0SAAAAAASO_2mDmdc4aaHP2p4jFBa6hhZ9",
 
 	/******** Develop environment ********/
 	dev: {
@@ -85,7 +63,10 @@ var configuration = {
 		
 		googleAppKey: "369163861143.apps.googleusercontent.com",
 		
-		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR"
+		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
+
+		/******** Google Analystic Relative ********/	
+		gaAccountId: "UA-50387624-1", // chewinggun.net - frank.hsu
 
 	},
 
@@ -105,7 +86,10 @@ var configuration = {
 		
 		googleAppKey: "369163861143.apps.googleusercontent.com",
 		
-		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR"
+		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
+
+		/******** Google Analystic Relative ********/	
+		gaAccountId: "UA-28254038-2", // chewinggun.net - frank.hsu
 
 	},
 
@@ -125,29 +109,16 @@ var configuration = {
 		
 		googleAppKey: "369163861143.apps.googleusercontent.com",
 		
-		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR"
+		googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
+
+		/******** Google Analystic Relative ********/	
+		gaAccountId: "UA-47307984-1", // cloud.sailtech.com
 
 	}
 
 }
 
-var dev = {
 
-
-}
-
-var test = {
-
-
-
-}
-
-
-var prod = {
-
-
-
-}
 
 
 
@@ -159,73 +130,30 @@ if(arguments.length > 2)
 console.log("Set enviroment >>> " + env);
 
 
-module.exports = {
-	
-	domainUrl : "https://cloud.sailstech.com",
+// Get utility config
+var returnConfig = {};
+for(var key in configuration) {
+	if( key!="dev" && key!="test" && key!="prod")		
+		returnConfig[key] = configuration[key];
+}
 
-	/******** Main resource folder  ********/		
-	resourcePath : "./resource/",
+// Get utility config (default prod)
+if(env == "dev") {
 
-	/******** Folder path of Building and floor files  ********/
-	imagePath : "public/client-image",
-	
-	mapInfoPath: "resource/map-info",
+	for( var key2 in configuration["dev"])
+		returnConfig[key2] = configuration["dev"][key2];
 
-	/******** Download sdk, online doc and sample code path ********/
+} else if(env == "test") {
 
-	sailsResPath: "resource/sails-relative-res",
+	for( var key2 in configuration["test"])
+		returnConfig[key2] = configuration["test"][key2];
 
-	androidDocPath: "resource/sails-relative-res/android/Javadoc.rar",		
+} else {
 
-	iosDocPath: "resource/sails-relative-res/ios/doc.rar",		
-		
-	defaultAndroidSdkVersion: 1.2,
+	for( var key2 in configuration["prod"])
+		returnConfig[key2] = configuration["prod"][key2];
 
-	defaultIosSdkVersion: 1.2,
+}
+console.log(returnConfig);
 
-
-	/******** Main sample building folder  ********/		
-	sampleBuildingPath: "resource/map-info/Sample",
-
-	/******** AppId and AppSecret of Facebook, twitter and google+ ********/	
-	facebookAppKey: "191009537749511",
-	
-	facebookAppSecret: "bd632d3cae63b4ad4314a7238dd2aa19",
-	
-	twitterAppKey: "T9Ovg7MAiehY4XUPTfZzw",
-	
-	twitterAppSecret: "zcB3JUdQlCslW7546OdropON116vENwx84E0K8bloI",
-	
-	googleAppKey: "369163861143.apps.googleusercontent.com",
-	
-	googleAppSecret: "GjyzGopNhamfve2Ouhw9firR",
-
-	/******** Default cookie duration ********/		
-	defaultCookieDuration: 604800, // 7 days: 604800 <=> 60 * 60 * 24 * 7
-
-	/******** Default token duration ********/	
-	defaultTokenDuration: 86400, // 1 day: 86400 <=> 60 * 60 * 24
-	
-	/******** Max number of building count ********/	
-	maxBuildingNumberOfUser: 50,
-
-	/******** Max number of floor count ********/	
-	maxFloorNumber: 100,
-	maxBasementNumber: 20,
-
-	/******** Pagination offset per page ********/	
-	pageOffset: 30,
-
-	/******** Google Analystic Relative ********/	
-	gaAccountIdTest1: "UA-50387624-1", // chewinggun.net - frank.hsu
-
-	gaAccountIdTest2: "UA-28254038-2", // chewinggun.net - frank.hsu
-
-	gaAccountId: "UA-47307984-1", // cloud.sailtech.com
-
-	/******** Recaptcha public and private key ********/
-	recaptchaPublicKey: "6Lco-u0SAAAAAM_vZInahzRKgLZMmHN_zJII7QU5",
-
-	recaptchaPrivateKey: "6Lco-u0SAAAAAASO_2mDmdc4aaHP2p4jFBa6hhZ9"
-
-};
+module.exports = returnConfig;
