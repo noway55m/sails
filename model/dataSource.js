0d6896dd062b9bb0f77ffbec81e42be3099fd7fd
frank.hsu <noway55m@gmail.com>
2014-05-11 22:43:39 +0800

    Add username and password with different env.

--- model/dataSource.js
+++ model/dataSource.js
@@ -1,3 +1,63 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/sails');
+var database = {
+
+	dev: {
+
+		uri: "mongodb://localhost:27017/sails",
+		options: {
+		  user: 'USERNAME',
+		  pass: 'PASSWORD'
+		}
+
+	},
+
+	test: {
+
+		uri: "mongodb://localhost:27017/sails",
+		options: {
+		  user: 'USERNAME',
+		  pass: 'PASSWORD'
+		}
+
+	},
+
+	prod: {
+
+		uri: "mongodb://localhost:27017/sails",
+		options: {
+		  user: 'USERNAME',
+		  pass: 'PASSWORD'
+		}
+
+	}
+
+};
+
+console.log("Database get config env: " + process.CONFIG_ENV);
+var env = process.CONFIG_ENV; 
+var uri = "";
+var options = "";
+if(env == "dev") {
+
+	uri = database.dev.uri;
+	options = database.dev.options;	
+
+} else if(env == "test") {
+
+	uri = database.test.uri;
+	options = database.test.options;	
+	
+} else {
+
+	uri = database.prod.uri;
+	options = database.prod.options;	
+
+}
+
+console.log("----------------- DATABASE " + env + "-----------------");
+console.log(uri);
+console.log(options);
+console.log("----------------- DATABASE " + env + "-----------------");
+
+mongoose.connect( uri, options );
 module.exports = mongoose;
