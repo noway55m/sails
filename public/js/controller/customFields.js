52c7f9bf0a1d1aa0ae3416977d41adedafbec880
frank.hsu <noway55m@gmail.com>
2014-07-15 21:37:29 +0800

    Add area and poi angular controller. Add customFields library for poi custom fields.

---
+++ public/js/controller/customFields.js
@@ -0,0 +1,302 @@
+function CustomFieldsGenerator(setting, $scope, $compile, Poi) {
+
+	var poi = $scope.poi,
+		poiClone = $scope.poiClone;
+
+	// Default setting
+	var defaultSetting = {
+		wrapperId: "customFieldsWrapper",
+		createButton: "customFieldsCreateButton",
+		typeMenu: "customFieldsTypeMenu",
+		displayArea: "customFieldsDisplayArea",
+		templateCustomField: "templateCustomField"
+	}
+
+	for(var key in setting){
+		if(setting[key])
+			defaultSetting = setting[key];
+	}
+
+	var me = this,
+		wrapper = $("#" + defaultSetting.wrapperId),
+		createButton = $("#" + defaultSetting.createButton),
+		typeMenu = $("#" + defaultSetting.typeMenu),
+		displayArea = $("#" + defaultSetting.displayArea),
+		templateCustomField = $("#" + defaultSetting.templateCustomField),
+		templateCustomField = $("#" + defaultSetting.templateCustomField),
+		templateCreateImage = $("#" + defaultSetting.templateCreateImage);
+	
+	// Function for toogle the field type menu
+	var typeMenuStatus = true;			
+	me.toogleTypeMenu = function(){
+		if(typeMenuStatus){
+			createButton.addClass("customFieldAddButtonToggle");
+			typeMenu.show();
+			typeMenuStatus = false;
+		} else {
+			createButton.removeClass("customFieldAddButtonToggle");			
+			typeMenu.hide();
+			typeMenuStatus = true;
+		}
+	}
+
+	// Event Handler for get the selected type and create field of this type
+	typeMenu.on("click", function(e){
+		// Hide type menu
+		me.toogleTypeMenu();
+
+		// Create field of specific type
+		var source = e.target || e.source;		
+		if(source.id.indexOf("Image") != -1){
+			me.createImageField();
+		} else if(source.id.indexOf("Link") != -1) {
+			me.createLinkField();
+		} else if(source.id.indexOf("Video") != -1) {
+			me.createVideoField();
+		} else if(source.id.indexOf("Audio") != -1) {		
+			me.createAudioField();
+		} else if(source.id.indexOf("File") != -1) {		
+			me.createFileField();
+		} else {
+			me.createTextField();
+		}
+	});
+
+	// Event Handler for toogle display and edit mode
+	$(document).on('click', 
+		".customFieldsButtonEdit, .templateCustomFieldsDisplay .customFieldsKey," +
+		 ".templateCustomFieldsDisplay .customFieldsValue", function(){
+		$(this).parent().hide();
+		$(this).parent().next().fadeIn();
+	}).on('click', ".customFieldsButtonCancel", function(){
+		$(this).parent().hide();
+		$(this).parent().prev().fadeIn();
+	});
+
+	// Function for get key number
+	function getNextKeyNumber(){
+		var result = poi.customFields.length; 
+		return result++;
+	}
+
+	// Function for show error message
+	function showErrorMsg(res){
+		var errorMsg = res && res.data && res.data.msg;
+		$().toastmessage('showErrorToast', errorMsg);					
+	}
+
+	// Function for show error message
+	function showSuccessMsg(type){
+		if(type == 'remove')
+			$().toastmessage('showSuccessToast', "Remove successfully");						
+		else 	
+			$().toastmessage('showSuccessToast', "Create successfully");			
+	}
+
+	// Function for sync to server
+	function syncToServer(poi, type){
+		poi.$save(function(){
+			poiClone = angular.copy(poi);
+			showSuccessMsg(type);
+		}, function(res){
+			angular.copy(poiClone, poi);
+			showErrorMsg(res);
+		});		
+	}
+
+	// ------------------------------------------------------------------------
+	
+	// Function for remove specific field
+	me.removeField = function(index){
+		poi.customFields.splice(index, 1);
+		syncToServer(poi, "remove");
+	}
+
+	// Function for cancel edit field
+	me.cancelField = function(){
+		angular.copy(poiClone, poi);
+	}
+
+	// Functon for update field
+	me.updateField = function(){
+		syncToServer(poi);
+	}
+
+	// ------------------------------------------------------------------------
+
+	// Function for create text field
+	me.createTextField = function(){
+		var currentCuout = getNextKeyNumber();		
+		$scope.$apply(function(){
+			poi.customFields.unshift({ 
+				key: "key" + currentCuout, 
+				value:"value" + currentCuout, 
+				type: 1 
+			});
+			syncToServer(poi);		
+		});		
+	}
+
+	// Function for toggle the display and edit mode of text field
+	// Since change line symbol is different between normal text and html
+	me.toggleMultipleLineMode = function(e, mode, field) {
+		var newValue = field.value;
+		if(mode) 	// edit -> display
+			newValue = newValue.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+		else 		// display -> edit
+			newValue = newValue.replace(/(<br\/>)/g, '\n');
+		field.value = newValue;	
+		
+		// Cancel edit need to rollback 
+		if(mode)
+			me.cancelField();
+	}
+
+	// ------------------------------------------------------------------------
+
+	// Function for create link field
+	me.createLinkField = function(){
+		var currentCuout = getNextKeyNumber();
+		$scope.$apply(function(){
+			poi.customFields.push({ 
+				key: "key" + currentCuout, 
+				value:"value" + currentCuout, 
+				type: 2
+			});
+			syncToServer(poi);
+		});		
+	}
+
+	// ------------------------------------------------------------------------
+
+	// Function for create image field
+	me.createImageField = function(){
+		var currentCuout = getNextKeyNumber();
+		$scope.$apply(function(){
+			poi.customFields.push({ 
+				key: "key" + currentCuout, 
+				value:"", 
+				type: 3,
+				displayValue :  "/img/no-image.png"
+			});
+			syncToServer(poi);
+		});
+	}
+
+
+	// Function for submit poi file(image, audio file)
+	me.submitPoiFile = function(e, obj, type){
+		
+		var field = obj,
+			saveButton = angular.element(e.currentTarget),
+			form = saveButton.prev(),
+			inputFields = form.find("input"),
+			errorMsgObj = form.find('.error-msg');
+
+		// Ajax from setup
+		var options = {
+
+			beforeSend : function(){ // pre-submit callback
+				inputFields.attr('disabled');
+				errorMsgObj.hide();
+				//saveButton.button("loading");
+				return true;
+			},
+			uploadProgress : function(event, position, total, percent){},
+			success : function(res, statusText){ // post-submit callback
+					
+				// Reset button
+				//saveButton.button("reset");
+
+				// Update field 
+				$scope.$apply(function () {
+					field.value = res;
+					syncToServer(poi);						
+				});
+
+				return true;
+			},
+			error : function(res, status){
+
+				console.log("33333");
+
+				// Reset button
+				//saveButton.button("reset");
+
+				// Show error msg
+				var resText = ( res.responseJSON && res.responseJSON.msg ) || "Fail to upload";
+				$().toastmessage('showErrorToast', resText );		        
+
+			},			
+
+			clearForm : true
+
+		};
+
+		form.ajaxSubmit(options);
+		return false;
+	}
+
+	// Event Handler about image field preview
+	$(document).on("change", "div.customFieldsValue.cimage input", function(e){
+		var image = this.files[0],
+			wrapper = $(this).parent();
+		// Check browser support file reader
+		if(window.FileReader){
+			// Load image and preview on parent div wrapper			
+			var reader = new FileReader();
+			reader.onload = function(theImage){
+				if(theImage && theImage.target)
+					wrapper.css("background", "url(" + theImage.target.result + ") no-repeat");
+			}
+			reader.readAsDataURL(image);
+		}
+	});
+
+	// ------------------------------------------------------------------------
+
+	// Function for create video field
+	me.createVideoField = function(){
+		var currentCuout = getNextKeyNumber();
+		$scope.$apply(function(){
+			poi.customFields.push({ 
+				key: "key" + currentCuout, 
+				value:"", 
+				type: 4
+			});
+			syncToServer(poi);
+		});		
+	}
+
+	// ------------------------------------------------------------------------
+
+	// Function for create audio field
+	me.createAudioField = function(){
+		console.log("fffee")
+		var currentCuout = getNextKeyNumber();
+		$scope.$apply(function(){
+			poi.customFields.push({ 
+				key: "key" + currentCuout, 
+				value:"", 
+				type: 5
+			});
+			syncToServer(poi);
+		});
+	}
+
+	// ------------------------------------------------------------------------
+
+	// Function for create file field
+	me.createFileField = function(){
+		var currentCuout = getNextKeyNumber();
+		$scope.$apply(function(){
+			poi.customFields.push({ 
+				key: "key" + currentCuout, 
+				value:"", 
+				type: 6
+			});
+			syncToServer(poi);
+		});		
+	}
+
+}
---
+++ public/js/controller/poiContrl.js
@@ -0,0 +1,604 @@
+var utility = Utility.getInstance();
+
+// List poi controller
+function PoiListCtrl($scope, Area, $compile, $rootScope, Poi) {
+
+	// Show and hide remove button
+	$scope.showRemoveButton = function(e){
+		angular.element(e.currentTarget).find(".remove-button-list").show();
+	};
+	
+	$scope.hideRemoveButton = function(e){
+		angular.element(e.currentTarget).find(".remove-button-list").hide();		
+	};	
+	
+    // List all pois
+	$scope.loading = true;
+	$scope.hasPagination = true;
+
+	// Load floor after building finish load
+	$rootScope.$on('areaFinishLoad', function(e, area) {
+
+		$scope.area = area;
+		Poi.list({ areaId: area._id }, function(obj){
+			
+			var page = obj.page,
+				offset = obj.offset,
+				count = obj.count,
+				totalPages = Math.ceil(count/offset),
+				pois = obj.pois;
+
+			// Set icon url
+			pois.forEach(function(poi){
+				if(poi.icon)
+					poi.icon = "/" + imagePath + "/" + poi.icon;
+				else
+					poi.icon = "/img/no-image.png";
+			});		
+			$scope.pois = pois;
+			$scope.loading = false;
+			
+			// Trigger pagination		
+			if( totalPages > 1 ) {
+
+				$scope.hasPagination = false;
+				$("#pagination").paginate({
+					count: totalPages,
+					start: 1,
+					display: 5,
+					border: false,
+					text_color: '#79B5E3',
+					background_color: 'none',	
+					text_hover_color: '#2573AF',
+					background_hover_color: 'none',
+					mouse: 'press'
+				});
+				$compile( angular.element('#pagination').contents() )($scope);
+
+			}
+
+			// Setup tooltip
+			setTimeout(function(){
+				tooltipSetup();
+			}, 500);
+
+		});
+
+	});
+
+	// Function for add new poi
+	$scope.addPoi = function(e) {
+
+		var addButton = angular.element(e.currentTarget),
+		    form = addButton.parent().prev(),
+			inputs = form.find("input"),
+			name = $(inputs[0]),
+			errorMsgObj = form.find('.error-msg');
+
+		// Clean error msg
+		errorMsgObj.hide();
+		errorMsgObj.find(".errorText").text("");
+
+		// Check format
+		if (utility.emptyValidate(name, errorMsgObj)) {
+
+			// Disable all fields and buttons
+			inputs.attr("disabled", "disabled");
+			addButton.button('loading');
+
+			// Create new poi
+			Poi.create({
+
+				name : name.val(),
+				areaId : $scope.area._id
+
+			}, function(poi) {
+
+				// Enable all fields and button
+				inputs.removeAttr("disabled").val("");
+				addButton.button("reset");
+
+				// Update local pois
+				if(poi.icon)
+					poi.icon = "/" + imagePath + "/" + poi.icon;
+				else
+					poi.icon = "/img/no-image.png";					
+				$scope.pois.push(poi);
+				
+				// Close dialog
+				$("#add-poi-dialog").modal("hide");
+
+		    	// Show success msg
+				$().toastmessage('showSuccessToast', "Create successfully");				
+
+			}, function(res) {
+
+				// Enable all fields and button
+				inputs.removeAttr("disabled");
+				addButton.button("reset");				
+
+				// Show error msg
+				var errorMsg = res && res.data && res.data.msg;
+				$().toastmessage('showErrorToast', errorMsg);						        
+
+			});
+
+		}
+
+	};
+
+	// Function for setup delete dialog
+	var deleteObj, deleteModal;
+	$scope.deleteDialogSetup = function(e){
+		deleteObj = this.poi;
+		deleteModal = $(e.currentTarget).parent().parent().next().next();
+		deleteModal.find("#removeContent").html(deleteObj.name);
+		deleteModal.modal("show");;
+	};
+	
+	// Function for delete ad obj
+	$scope.deleteObj = function(e){
+		
+		// Hide modal
+		deleteModal.modal('hide');
+		
+		// Delete poi
+		Poi.delete({
+			
+			_id: deleteObj._id
+			
+		}, function(res){
+
+			if(res._id){
+				
+				// Remove store from view
+		    	var id = res._id;
+		    	for(var i=0; i<$scope.pois.length; i++){			
+					if($scope.pois[i]._id == id){    			
+						$scope.pois.splice(i, 1);
+						break;
+					}
+		    	}
+		    	
+		    	// Show success msg
+				$().toastmessage('showSuccessToast', "Remove successfully");
+				
+			}			
+			
+		}, function(res){
+
+			// Show error msg
+			var errorMsg = res && res.data && res.data.msg;
+			$().toastmessage('showErrorToast', errorMsg);						        
+
+		});
+		
+	};
+
+	// Function for load specific page
+	$scope.loadPage = function(e, page){
+
+		var element = angular.element(e.currentTarget);
+		Poi.list({ page: page }, function(obj){
+			
+			var page = obj.page,
+				offset = obj.offset,
+				count = obj.count,
+				pois = obj.pois;
+
+			$scope.pois = pois;
+
+		});
+
+	};
+
+	// Get poi copy and copy template
+	$scope.isAreaShowPage = false;
+	if(window.location.toString().indexOf("/area/show")!=-1)
+		$scope.isAreaShowPage = true;		
+	Poi.getCopies(function(copyPoiList){
+		$scope.copyPoiList = copyPoiList;
+	});
+	Poi.getCopyTemplates(function(copyPoiTemplateList){
+		$scope.copyPoiTemplateList = copyPoiTemplateList;		
+	});
+
+
+	// Function for load specific page		
+	$scope.copyPoi = function(e){
+		copyPoi(e, $scope, Poi, this.poi);
+	}
+
+	// Function for load specific page	
+	$scope.copyPoiTemplate = function(e, poi){
+		copyPoiTemplate(e, $scope, Poi, this.poi);		
+	}
+
+	// Function for copy on current area
+	$scope.copyPoiHere = function(e, poi){
+
+      	var areaId = $scope.area._id,
+      		areaName = $scope.area.name,
+      		poiId = angular.element(e.currentTarget).parent().parent().attr("id"),
+      		selectPoi = {};
+
+      	// Check come from copy or copy template and clone the POI
+     	angular.copy(poi, selectPoi);      		
+      	if(poiId.indexOf("copy-template")!=-1) {
+			for(var key in selectPoi) {
+				console.log(typeof selectPoi[key]);
+				if(typeof selectPoi[key] == "string" && key != "name") {
+					selectPoi[key] = ""
+				} else if(typeof selectPoi[key] == "object") {
+					selectPoi[key] = [];
+				}
+			}
+      	}
+      	selectPoi.areaId = areaId;
+      	Poi.create(selectPoi, function(thePoi){
+
+      		// Update poi list
+			$scope.pois.push(thePoi);
+	    	
+	    	// Show success msg
+			$().toastmessage('showSuccessToast', "Copy POI to " +  areaName + " successfully");				
+
+      	}, function(res){
+
+			// Show error msg
+			var errorMsg = res && res.data && res.data.msg;
+			$().toastmessage('showErrorToast', errorMsg);						        
+
+      	});
+
+	}
+
+	// Function for remove copy poi
+	$scope.removeCopyPoi = function(e, poi, index){
+
+		var poiId = angular.element(e.currentTarget).parent().parent().attr("id");
+      	if(poiId.indexOf("copy-template")!=-1) {
+
+      		Poi.removeCopyTemplate({ index: index }, function(copyPoi){
+
+      			// Update copy poi list
+      			$scope.copyPoiTemplateList = copyPoi;
+
+		    	// Show success msg
+				$().toastmessage('showSuccessToast', "Remove copy poi successfully");
+
+      		}, function(res) {
+
+				// Show error msg
+				var errorMsg = res && res.data && res.data.msg;
+				$().toastmessage('showErrorToast', errorMsg);						        
+
+      		});
+
+      	} else {
+
+      		Poi.removeCopy({ index: index }, function(copyPoi){
+
+      			// Update copy poi list
+      			$scope.copyPoiList = copyPoi;
+
+		    	// Show success msg
+				$().toastmessage('showSuccessToast', "Remove copy poi template successfully");
+
+      		}, function(res) {
+
+				// Show error msg
+				var errorMsg = res && res.data && res.data.msg;
+				$().toastmessage('showErrorToast', errorMsg);						        
+      			
+      		});
+
+      	}
+
+	}
+
+}
+
+// Show specific poi controller
+function PoiShowCtrl($rootScope, $scope, $location, $compile, Poi, Area, Building, User) {
+	var url = $location.absUrl(),
+		id = url.substring(url.lastIndexOf("/") + 1, url.length) || $rootScope.selectedBuilding.id;	
+	$scope.loadingPoi = true;
+	Poi.get({ _id : id }, function(poi){
+		
+		// Assign custom fields by default
+		$scope.poi = poi;
+
+		// Construct custom fields area
+		for(var key in poi.customFields){ // handle the file undefined
+			var thePoi = poi.customFields[key],
+				type = thePoi.type,
+			 	value = thePoi.value;			
+			// image type
+			if(type == 3){
+				if(!value)
+					thePoi.displayValue = "/img/no-image.png";
+				else
+					thePoi.displayValue = "/poi/getFile?type=image&filePath=" + value;
+			}
+		}
+
+        $scope.poiClone = angular.copy(poi); // Clone area for future rollback        
+    	$scope.loadingPoi = false;
+
+		// Get area of poi
+		Area.get({ _id: poi.areaId }, function(area){
+			$scope.area = area;
+		});
+
+		// Get building of poi and user's building
+		Building.get({ _id: $scope.poi.buildingId }, function(building){
+			$scope.poiBuilding = building;
+			$scope.poiBuildingClone = building;
+		}, function(){
+			$scope.poiBuilding = { name: "Binding Building 2222" };
+			$scope.poiBuildingClone = $scope.poiBuilding;
+		});
+
+		Building.list(function(obj){
+			$scope.buildings = obj.buildings ? obj.buildings : [];
+		});		
+
+		// Get user's tags for poi tag autocomplete
+		User.poiTags({}, function(poiTags){
+			
+			var userTags = [];
+			for(key in poiTags)
+				userTags[key] = poiTags[key].data;
+
+			var substringMatcher = function(strs) {
+			  return function findMatches(q, cb) {
+			    var matches, substringRegex;
+			 
+			    // an array that will be populated with substring matches
+			    matches = [];
+			 
+			    // regex used to determine if a string contains the substring `q`
+			    substrRegex = new RegExp(q, 'i');
+			 
+			    // iterate through the pool of strings and for any string that
+			    // contains the substring `q`, add it to the `matches` array
+			    $.each(strs, function(i, str) {
+			      if (substrRegex.test(str)) {
+			        // the typeahead jQuery plugin expects suggestions to a
+			        // JavaScript object, refer to typeahead docs for more info
+			        matches.push({ value: str });
+			      }
+			    });
+			 
+			    cb(matches);
+			  };
+			};
+			
+			// Setup tags
+			var	poiTags = $scope.poi.tags;
+
+			// Bootstrap tag manager library
+			var tagApi = $("#poi-tag-input").tagsManager({
+				prefilled: poiTags
+			});
+
+			// Twitter typehead autocomplete setup
+			tagApi.typeahead({ 
+				hint: true,
+				highlight: true,
+				minLength: 1
+			},{
+				name: 'tags',
+				displayKey: 'value',
+				source: substringMatcher(userTags)
+			}).on('typeahead:selected', function (e, d) {		 
+				
+				// Select autocomplete dialog then push new tag	
+				tagApi.tagsManager("pushTag", d.value);		 
+		    
+		    });
+
+			// Event handler for check tags value change
+		    $("input[name=hidden-" + tagApi.attr("name") + "]").on('change', function(v, nv){
+		    	$(".tagsButtonSave").show();
+		    	$(".tagsButtonCancel").show();
+		    });
+
+		});
+	
+		// Set clipboard forp copy poi id
+		clipboardSetup();
+		
+		$scope.cfg = new CustomFieldsGenerator({}, $scope, $compile, Poi);
+
+		// Setup tooltip
+		tooltipSetup();
+
+	});
+	
+	// Function for show error message
+	function showErrorMsg(res){
+		var errorMsg = res && res.data && res.data.msg;
+		$().toastmessage('showErrorToast', errorMsg);					
+	}
+
+	// Function for show error message
+	function showSuccessMsg(){
+		$().toastmessage('showSuccessToast', "Update successfully");			
+	}
+
+	// Function for toogle name block
+	$scope.toogleNameBlock = function(mode){
+		if(mode) {
+			$(".nameDisplayBlock").fadeIn();
+			$(".nameEditBlock").hide();		
+		} else {
+			$(".nameDisplayBlock").hide();
+			$(".nameEditBlock").fadeIn();					
+		}
+	}
+
+	// Function for update poi name
+	$scope.update = function(poiObj, successCallback, errorCallback){
+		var poi = this.poi || poiObj;
+		poi.$save(function(){
+			$scope.toogleNameBlock(1);
+			showSuccessMsg();
+			if(successCallback)
+				successCallback();
+		}, function(res){
+			showErrorMsg(res);
+			if(errorCallback)
+				errorCallback();			
+		});
+	}
+
+	// Function for cancel update poi name
+	$scope.cancelUpdate = function(){
+		angular.copy($scope.poiClone, $scope.poi);
+		$scope.toogleNameBlock(1);
+	}
+
+	// Function for bind building
+	$scope.bindBuilding = function(e, building){
+		var poi = this.poi;
+		poi.buildingId = building._id;
+		$scope.update(poi, function(){
+			$scope.poiBuilding = building;
+		}, function(){
+			angular.copy($scope.poiClone, poi);
+		});
+	}
+
+	// Function for cancel update poi tags
+	$scope.tagSave = function(e){
+		var poi = this.poi;
+		poi.tags =	$("#poi-tag-input").tagsManager("tags");		
+		$scope.update(poi, function(){
+		}, function(){
+			angular.copy($scope.poiClone, poi);			
+		});		
+	}
+
+	// Function for cancel update poi tags
+	$scope.tagCancel = function(e){
+		var poi = this.poi,
+			poiTags = poi.tags && poi.tags.indexOf(",")!=-1 ? poi.tags.split(",") : [],
+			poiTagInput = $("#poi-tag-input");
+		
+		// Empty tags first
+		poiTagInput.tagsManager("empty");
+
+		// Push original tags
+		for(var key in poiTags)
+			poiTagInput.tagsManager("pushTag", poiTags[key]);
+	    
+		// Hide buttons
+	    $(".tagsButtonSave").hide();
+	    $(".tagsButtonCancel").hide();
+	}
+		
+}
+
+// Function for copy poi
+function copyPoi(e, $scope, Poi, poi){
+	Poi.copy( poi, function(copyPoi) {
+
+		// Put copy list		
+		$scope.copyPoiList = copyPoi;
+
+		// Change tooltip title
+  		var poiId = angular.element(e.currentTarget),
+  			poiIdCopyTooltipTitle = "Copy poi";
+  		poiId.tooltip("destroy");
+  		poiId.tooltip({
+  			title: "Copied"
+  		});
+		poiId.tooltip("show");
+		setTimeout(function(){
+			poiId.one('hide.bs.tooltip', function(e){
+		  		poiId.tooltip("hide");
+		  		poiId.tooltip("destroy");
+		  		poiId.tooltip({
+		  			title: poiIdCopyTooltipTitle
+		  		});
+			});	
+		}, 10);
+
+	}, function(){
+
+	});
+}
+
+// Function for copy poi tempate
+function copyPoiTemplate(e, $scope, Poi, poi){
+	Poi.copyTemplate( poi, function(copyPoi) {		
+		
+		// Put copy template list
+		$scope.copyPoiTemplateList = copyPoi;
+
+		// Change tooltip title
+  		var poiId = angular.element(e.currentTarget),
+  			poiIdCopyTooltipTitle = "Copy poi template";
+  		poiId.tooltip("destroy");
+  		poiId.tooltip({
+  			title: "Copied"
+  		});
+		poiId.tooltip("show");
+		setTimeout(function(){
+			poiId.one('hide.bs.tooltip', function(e){
+		  		poiId.tooltip("hide");
+		  		poiId.tooltip("destroy");
+		  		poiId.tooltip({
+		  			title: poiIdCopyTooltipTitle
+		  		});
+			});	
+		}, 10);
+
+	}, function(){
+
+	});
+}
+
+// Function for setup poi id copy clipboard by zeroclipboard
+var poiIdCopyTooltipTitle = "Copy id to tooltip";
+function clipboardSetup(){
+	var client = new ZeroClipboard( document.getElementById("poiId") );
+	client.on( "ready", function( readyEvent ) {
+	  client.on( "aftercopy", function( event ) {
+	  		var poiId = $('#poiId'),
+	  			poiIdCopyTooltipTitle = "Copy id to tooltip";
+	  		poiId.tooltip("destroy");
+	  		poiId.tooltip({
+	  			title: "Copied"
+	  		});
+			poiId.tooltip("show");
+			setTimeout(function(){
+				poiId.one('hide.bs.tooltip', function(e){
+			  		poiId.tooltip("hide");
+			  		poiId.tooltip("destroy");
+			  		poiId.tooltip({
+			  			title: poiIdCopyTooltipTitle
+			  		});
+				});	
+			}, 10)
+	  });
+	});	
+}
+
+// Function for setup tooltip
+function tooltipSetup(){
+
+	// Tooltip of copy poi and copy poi template
+	$('.copy:not(#copy-poi-trigger-button span)').tooltip( { title: "Copy poi" } );
+	$('.copy-template:not(#copy-poi-template-trigger-button span)').tooltip( { title: "Copy poi template" } );
+
+
+	// Tooltip of copy poi id to clipboard 
+	$('#poiId').tooltip({ title: poiIdCopyTooltipTitle });	
+
+
+}
+
+// PoiShowCtrl.$inject = ['$scope', 'Poi'];
